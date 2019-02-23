4406c1928d3a5a4f6806e6b1665576b2a840f25e
Pinecone628 <aurorawu1996@gmail.com>
2019-02-22 20:15:16 -0600

    New

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -30,3 +30,58 @@ class Tree(object):
                 return
             else:
                 queue.append(cur_node.rchild)
+
+    def bread_travel(self):
+        if self.root is None:
+            return
+        queue = [self.root]
+        while queue:
+            cur_node = queue.pop(0)
+            print(cur_node.elem, end="")
+            if cur_node.lchild is not None:
+                queue.append(cur_node.lchild)
+            if cur_node.rchild is not None:
+                queue.append(cur_node.rchild)
+
+    def preorder(self, node):
+
+        if node is None:
+            return
+        print(node.elem, end="")
+        self.preorder(node.lchild)
+        self.preorder(node.rchild)
+
+    def inorder(self, node):
+
+        if node is None:
+            return
+        self.inorder(node.lchild)
+        print(node.elem, end="")
+        self.inorder(node.rchild)
+
+    def postorder(self, node):
+
+        if node is None:
+            return
+        self.postorder(node.lchild)
+        self.postorder(node.rchild)
+        print(node.elem, end="")
+
+
+if __name__ == "__main__":
+    tree = Tree()
+    tree.add(0)
+    tree.add(1)
+    tree.add(2)
+    tree.add(3)
+    tree.add(4)
+    tree.add(5)
+    tree.add(6)
+    tree.add(7)
+    tree.add(8)
+    tree.add(9)
+    tree.preorder(tree.root)
+    print("")
+    tree.inorder(tree.root)
+    print("")
+    tree.postorder(tree.root)
